5e367a399daf71792e4a41c73ebee7f46bd82284
kamal <kamal.chandra@gmail.com>
2021-03-22 15:25:41 +0530

    spring

--- src/main/java/net/codejava/spring/controller/HomeController.java
+++ src/main/java/net/codejava/spring/controller/HomeController.java
@@ -13,6 +13,7 @@ public class HomeController {
 
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
+		
 		return new ModelAndView("home");
 	}
 }
